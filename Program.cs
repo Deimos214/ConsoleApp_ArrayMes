ffe99cec5826dd34f9f754aaddb1f5dba285d939
Usuario <Usuario@CPD03-M07>
2024-04-30 10:52:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ArrayMes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mes = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio" };
+
+            Console.WriteLine("Digite o número do mês que deseja de 0 a 4: ");
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O mês escolhido foi: " + mes[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
